de238039beafa705f09d6bb18d21855c5e5106a1
Mert Genç <162042618+Mertgencc@users.noreply.github.com>
2026-05-07 01:32:40 +0300

    Ekran ilk açılınca verilerin yüklenmesi ve kullanıcının seçtiği kategorilerin gelmesi hazırlandı

--- js/main7.js
+++ js/main7.js
@@ -2,3 +2,16 @@ let film = [];
 let selectedCategory = "Hepsi";
 let editID = null;
 
+window.onload = function(){
+    const data = localStorage.getItem("film");
+    if(data){
+        film = JSON.parse(data);
+    }
+
+    document.getElementById("category").onchange = function(e){
+        selectedCategory = e.target.value;
+        renderFilm();
+    }
+
+    renderFilm();
+}
