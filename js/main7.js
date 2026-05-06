70c30d4f5d7089a270ae7a164b07c7a1c712ec49
Mert Genç <162042618+Mertgencc@users.noreply.github.com>
2026-05-07 01:40:35 +0300

    Ekleme fonksiyonumuz oluşturuldu, idye göre edit mantığını kurduk, itemlar oluşturduk

--- js/main7.js
+++ js/main7.js
@@ -20,3 +20,42 @@ function generateID(){
     return Date.now;
 }
 
+function addFilm(){
+    const name = document.getElementById("nameInput");
+    const type = document.getElementById("typeInput");
+    const category = document.getElementById("categoryInput");
+
+    name = nameInput.value;
+    type = typeInput.value;
+    category = categoryInput.value;
+
+    if(name === " " || type === " ") return;
+
+    if(editID){
+        const item = film.find((f) => f.id === editID);
+
+        item.name = name;
+        item.type = type;
+        item.category = category;
+
+        editID = null;
+    } else{
+        const newItem ={
+            id: generateID(),
+            name: name,
+            type: type,
+            category: category,
+            completed: false,
+        }
+
+        film.push(newItem);
+    }
+
+    localStorage.setItem("film", JSON.stringify(film));
+
+    renderFilm();
+
+    nameInput.value = "";
+    typeInput.value = "";
+    
+}
